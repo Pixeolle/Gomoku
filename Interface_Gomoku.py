9252b6eb6d02680f27b8a15133b12f1082bd493d
Raphael <raphaelbaran@orange.fr>
2024-12-04 02:14:14 +0100

    Ajout nouvelle police, numérotation lignes/colonnes

--- Interface_Gomoku.py
+++ Interface_Gomoku.py
@@ -1,6 +1,6 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout, QLabel, QMessageBox, QSpacerItem, QSizePolicy
-from PyQt5.QtGui import QPainter, QColor, QPen
+from PyQt5.QtGui import QPainter, QColor, QPen, QFont, QFontDatabase
 from PyQt5.QtCore import Qt, QPoint
 from board import Board
 import random
@@ -14,8 +14,10 @@ class GomokuGUI(QMainWindow):
         self.setWindowTitle('Gomoku Game')
         self.resize(800, 800)
         self.board = Board(15, 15)
+        QFontDatabase.addApplicationFont("BebasNeue-Regular.ttf")
         self.current_player = random.randint(1, 2)
         self.central_widget = QWidget()
+        self.central_widget.setStyleSheet('background-color: #c8b496;')
         self.setCentralWidget(self.central_widget)
         self.layout = QVBoxLayout(self.central_widget)
         self.label = QLabel(f'Player {self.current_player}\'s turn', self)
@@ -49,7 +51,7 @@ class GameBoard(QWidget):
         self.board = board
         self.parent = parent
         self.cell_size = 40
-        self.grid_marge = 40
+        self.grid_marge = 60
         self.update_board_size()
 
     def update_board_size(self):
@@ -64,8 +66,10 @@ class GameBoard(QWidget):
     def paintEvent(self, event):
         qp = QPainter()
         qp.begin(self)
-        self.paint_background(qp)
+        #self.paint_background(qp)
         self.draw_board(qp)
+        self.draw_pieces(qp)
+        self.draw_labels(qp)
         qp.end()
 
     def paint_background(self, qp):
@@ -82,6 +86,22 @@ class GameBoard(QWidget):
             x = j * self.cell_size + self.grid_marge
             qp.drawLine(x, self.grid_marge, x, self.height() - self.grid_marge)
 
+    def draw_labels(self, qp):
+        qp.setPen(QPen(Qt.black, 1, Qt.SolidLine))
+        qp.setFont(QFont('Bebas Neue', 15))
+        for i in range(self.board.height):
+            letter = chr(ord('A') + i)
+            y = i * self.cell_size + self.grid_marge
+            qp.drawText(20, y + 5, letter)
+            qp.drawText(self.width() - 30, y + 5, letter)
+
+        for j in range(self.board.width):
+            x = j * self.cell_size + self.grid_marge
+            qp.drawText(x - 5, self.grid_marge - 25, str(j))
+            qp.drawText(x - 5, self.height() - 25, str(j))
+
+    def draw_pieces(self, qp):
+        qp.setRenderHint(QPainter.Antialiasing)
         for i in range(self.board.height):
             for j in range(self.board.width):
                 x0 = j * self.cell_size + self.grid_marge
@@ -89,9 +109,11 @@ class GameBoard(QWidget):
                 bit_position = self.board.get_x_y(self.board.position, i, j)
                 if bit_position == 1:
                     qp.setBrush(QColor(255, 255, 255))
-                    qp.drawEllipse(QPoint(x0, y0), self.cell_size // 2 - 5, self.cell_size // 2 - 5 )
+                    qp.setPen(Qt.NoPen)
+                    qp.drawEllipse(QPoint(x0, y0), self.cell_size // 2 - 5, self.cell_size // 2 - 5)
                 elif self.board.get_x_y(self.board.mask, i, j) == 1:
                     qp.setBrush(QColor(0, 0, 0))
+                    qp.setPen(Qt.NoPen)
                     qp.drawEllipse(QPoint(x0, y0), self.cell_size // 2 - 5, self.cell_size // 2 - 5)
                 qp.setBrush(Qt.NoBrush)
 
@@ -102,12 +124,11 @@ class GameBoard(QWidget):
         y -= self.grid_marge
         grid_x = round(x / self.cell_size)
         grid_y = round(y / self.cell_size)
-        snap_x = grid_x * self.cell_size
-        snap_y = grid_y * self.cell_size
+        aligne_x = grid_x * self.cell_size
+        aligne_y = grid_y * self.cell_size
 
         seuil = self.cell_size // 4
-
-        if (abs(x - snap_x) <= seuil and abs(y - snap_y) <= seuil and grid_x < self.board.width and grid_y < self.board.height):
+        if (abs(x - aligne_x) <= seuil and abs(y - aligne_y) <= seuil and grid_x < self.board.width and grid_y < self.board.height):
             position = chr(ord('A') + grid_y) + str(grid_x)
             try:
                 self.board.play_to(self.parent.current_player, position)
